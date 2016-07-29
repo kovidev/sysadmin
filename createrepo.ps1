1a9f0aea6a250166614a014ec4778ab161500635
Taemin Hong <taemin@kovi.com>
2016-07-29 16:49:21 +0900

    주어진 이름의 저장소가 이미 있으면 생성 요청을 거부합니다.
    저장소 생성 스크립트를 배치파일에서 파워셸로 바꿨습니다.
    파워셸 스크립트를 실행하려면 실행 정책을 적절한 값으로 변경해야 합니다.
    자세한 내용은 아래 링크를 참조하십시오.
    
    https://technet.microsoft.com/en-us/library/ee176961.aspx

---
+++ createrepo.ps1
@@ -0,0 +1,36 @@
+param (
+	[ValidateNotNullorEmpty()]
+	[string]$RepositoryRoot = "E:\KoviProjectSystem\SvnRepo",
+
+	[parameter(Mandatory=$true)]
+	[ValidateNotNullorEmpty()]
+	[string]$RepositoryName,
+
+	[switch]$Empty = $false
+)
+
+# Check that a directory with given name already exists.
+$RepositoryPath = Join-Path $RepositoryRoot $RepositoryName
+if (Test-Path $RepositoryPath) {
+	throw "$RepositoryPath already exists"
+}
+
+# Initialize the repository
+svnadmin create --pre-1.5-compatible $RepositoryPath >$null
+robocopy /IS /E repository-skeleton $RepositoryPath >$null
+
+if (!$Empty.IsPresent) {
+	# Construct the standard directory structure and commit.
+	$TempDir = [System.Guid]::NewGuid().ToString()
+	New-Item -Type Directory -Name $TempDir >$null
+	cd $TempDir
+
+	svn co -q svn://localhost/$RepositoryName
+	cd $RepositoryName
+	svn mkdir -q trunk branches tags
+	svn ci -q -m "Repository initialized."
+
+	# Cleanup
+	Set-Location ..\..
+	Remove-Item -Force -Recurse $TempDir
+}
